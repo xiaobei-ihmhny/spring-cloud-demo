0e2261fb2972956c04a8366ec3cfa9e462c2aa4b
zhibeibei <Z471200a>
2020-04-26 14:13:47 +0800

    添加eureka服务发现组件配置，实现服务化配置中心

--- config-client/src/main/java/com/spring/cloud/demo/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/cloud/demo/configclient/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.spring.cloud.demo.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient// 用来发现config-server服务
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
--- config-server/src/main/java/com/spring/cloud/demo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/spring/cloud/demo/configserver/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.spring.cloud.demo.configserver;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient// 将config-server注册到服务注册中心上去
 public class ConfigServerApplication implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
