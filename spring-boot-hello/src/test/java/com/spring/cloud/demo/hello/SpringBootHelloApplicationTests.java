e549f6154e9ff6d723d99f48c157668a08ce056a
zhibeibei <Z471200a>
2020-05-08 14:08:27 +0800

    spring-boot-hello测试项目进行正常的Mock测试

--- spring-boot-hello/src/test/java/com/spring/cloud/demo/hello/SpringBootHelloApplicationTests.java
+++ spring-boot-hello/src/test/java/com/spring/cloud/demo/hello/SpringBootHelloApplicationTests.java
@@ -1,35 +1,33 @@
 package com.spring.cloud.demo.hello;
 
-import com.spring.cloud.demo.hello.web.HelloController;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// TODO 测试类编写学习
+/**
+ * TODO 测试类编写学习
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020-05-08 13:44:44
+ */
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootHelloApplicationTests {
 
-    private MockMvc mockMvc;
-
-    @BeforeAll
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-    }
+    @Autowired
+    public MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello")
-                .accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("hello World")));
+                .andExpect(content().string(equalTo("Hello World")));
     }
 
     @Test
