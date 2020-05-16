89332e58dc2ee204da2a852c9f3f7ee6a3afa4eb
xiaobei <legend0508@163.com>
2020-05-16 15:50:37 +0800

    spring boot hello 学习完成

--- spring-boot-hello/src/main/java/com/spring/cloud/demo/hello/SpringBootHelloApplication.java
+++ spring-boot-hello/src/main/java/com/spring/cloud/demo/hello/SpringBootHelloApplication.java
@@ -1,7 +1,10 @@
 package com.spring.cloud.demo.hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author xiaobei
@@ -13,4 +16,8 @@ public class SpringBootHelloApplication {
         SpringApplication.run(SpringBootHelloApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository traceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
