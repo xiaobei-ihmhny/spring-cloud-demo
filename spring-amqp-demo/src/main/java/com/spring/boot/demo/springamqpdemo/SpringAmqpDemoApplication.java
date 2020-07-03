1a07411231c1ce5bce071ee1a634246ddf5185eb
zhibeibei <Z471200a>
2020-07-03 14:14:27 +0800

    创建rabbitmq测试工程

---
+++ spring-amqp-demo/src/main/java/com/spring/boot/demo/springamqpdemo/SpringAmqpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.demo.springamqpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmqpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAmqpDemoApplication.class, args);
+    }
+
+}
